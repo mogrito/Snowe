1e46b03e8c9befec17c08255813592b9d1861096
hong <juseong0625@gmail.com>
2023-09-21 12:20:33 +0900

    게시판 컨트롤러

--- src/main/java/com/capstone/snowe/controller/BoardController.java
+++ src/main/java/com/capstone/snowe/controller/BoardController.java
@@ -3,16 +3,22 @@ package com.capstone.snowe.controller;
 import com.capstone.snowe.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controller
+@Controller("/board")
 public class BoardController {
     @Autowired
     private BoardService boardService;
 
-    @RequestMapping(value = "/board", method = RequestMethod.GET)
+    @RequestMapping(value = "/board/main", method = RequestMethod.GET)
     public String home() {
         return "board/boardMain";
+    }
+
+    @GetMapping(value = "/board/boardWrite")
+    public String boardWrite() {
+        return "board/boardWrite";
     }
 }
